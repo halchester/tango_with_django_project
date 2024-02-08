2183268caa8ff2f2189f7e2904de7d1e1968ce75
Htoo Lynn <2819922l@student.gla.ac.uk>
2024-02-08 15:22:36 +0000

    feat: chapter 10

--- rango/views.py
+++ rango/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm, UserProfileForm, UserForm
+from datetime import datetime
 
 
 def index(request):
@@ -17,12 +18,18 @@ def index(request):
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
 
-    return render(request, 'rango/index.html', context=context_dict)
+    response = render(request, 'rango/index.html', context=context_dict)
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+    return response
 
 
 def about(request):
     context_dict = {'message': "This tutorial has been put together by Htoo"}
 
+    visitor_cookie_handler(request)
+    visits = request.session['visits']
+    context_dict['visits'] = visits
     return render(request, 'rango/about.html', context=context_dict)
 
 
@@ -142,3 +149,28 @@ def restricted(request):
 def user_logout(request):
     logout(request)
     return redirect(reverse('rango:index'))
+
+
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+# Updated the function definition
+
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(
+        request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(
+        last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+# Update the last visit cookie now that we have updated the count request.session['last_visit'] = str(datetime.now())
+    else:
+        # Set the last visit cookie
+        request.session['last_visit'] = last_visit_cookie
+        # Update/set the visits cookie
+    request.session['visits'] = visits
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -135,3 +135,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 LOGIN_URL = 'rango:login'
+
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
